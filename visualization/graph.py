7f59e4767db204baac0dcc9121f294d30204b4da
Justin Cosentino <justintcosentino@gmail.com>
2019-03-24 16:36:33 +0800

    Add visualization methods for l1 weight norms and l2 unit norms

--- visualization/graph.py
+++ visualization/graph.py
@@ -16,10 +16,9 @@ def plot_history(history, experiment_dir, output_name='loss_accuracy.png'):
     accuracy and loss over epochs and writes as a png to the specified
     experiment directory.
     """
-    plt.figure(figsize=(16, 4))
-    plt.subplots_adjust(hspace=0.5)
+    plt.figure(figsize=(12, 4))
 
-    plt.subplot(121)
+    plt.subplot(1, 2, 1)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
     plt.xlabel('Epoch')
@@ -27,12 +26,53 @@ def plot_history(history, experiment_dir, output_name='loss_accuracy.png'):
     plt.legend(['train', 'validation'], loc='upper right')
     plt.xticks(np.arange(0, FLAGS.epochs, step=1))
 
-    plt.subplot(122)
+    plt.subplot(1, 2, 2)
     plt.plot(history.history['acc'])
     plt.plot(history.history['val_acc'])
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.legend(['train', 'validation'], loc='upper left')
     plt.xticks(np.arange(0, FLAGS.epochs, step=1))
+
+    plt.savefig(os.path.join(experiment_dir, output_name))
+
+
+def plot_weights_l1(model, experiment_dir, output_name="weights.png"):
+    """
+    Plot the distribution of l1 weight norms for the model's current weights.
+    Save the plot as a png in the specified directory.
+    """
+    plt.figure(figsize=(12, 4))
+    plt.subplots_adjust(hspace=0.8, wspace=0.4)
+
+    for i, layer in enumerate(model.layers):
+        name = layer.get_config()['name']
+        weights = np.absolute(layer.get_weights()[0])
+        plt.subplot(2, 3, i+1)
+        plt.title(name)
+        plt.xlabel('L1 Weights Norm')
+        plt.ylabel('Frequency')
+        plt.hist(weights.reshape(-1), bins=300)
+
+    plt.savefig(os.path.join(experiment_dir, output_name))
+
+
+def plot_units_l2(model, experiment_dir, output_name="units.png"):
+    """
+    Plot the distribution of l2 unit norms for the model's current weights.
+    Save the plot as a png in the specified directory.
+    """
+    plt.figure(figsize=(12, 4))
+    plt.subplots_adjust(hspace=0.8, wspace=0.4)
+
+    for i, layer in enumerate(model.layers):
+        name = layer.get_config()['name']
+        weights = layer.get_weights()[0]
+        column_l2 = np.linalg.norm(weights, ord=2, axis=0)
+        plt.subplot(2, 3, i+1)
+        plt.title(name)
+        plt.xlabel('L2 Unit Norm')
+        plt.ylabel('Frequency')
+        plt.hist(column_l2, bins=100)
 
     plt.savefig(os.path.join(experiment_dir, output_name))
