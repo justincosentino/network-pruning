318adf3e02df032b582b4aeb12c13f1f3b5eb37e
Justin Cosentino <justintcosentino@gmail.com>
2019-03-26 11:15:24 +0800

    Fix mismatch

--- visualization/csv.py
+++ visualization/csv.py
@@ -20,10 +20,10 @@ def write_to_csv(
     pd.options.display.float_format = '{:2.4f}'.format
     frame = pd.DataFrame(data={
         "Sparsity (%)": k_vals,
-        "Test Accuracy (Unit Pruning)": weight_accuracies,
-        "Test Accuracy (Weight Pruning)": unit_accuracies,
-        "Test Loss: (Weight Pruning)": weight_losses,
-        "Test Loss: (Unit Pruning)": unit_losses,
+        "Test Accuracy (Weight)": weight_accuracies,
+        "Test Accuracy (Unit)": unit_accuracies,
+        "Test Loss: (Weight)": weight_losses,
+        "Test Loss: (Unit)": unit_losses,
     })
     frame.to_csv(
         os.path.join(experiment_dir, output_name),
